b9014fc66755dd63948cf4f5ed4c4fa68466f2f6
EmilyLow <emlow@me.com>
2020-07-15 19:33:18 -0500

    Submitting working and non-checkbox fields working

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, {useState, useEffect } from "react";
 import * as yup from "yup";
 import axios from "axios";
 
@@ -7,19 +7,93 @@ import axios from "axios";
 
 function Form() {
 
-    const handleChange = {};
-    const onSubmit = {};
-
+   
     const defaultState = {
         name: "",
         email: "",
         password:"",
-        terms: false,
+        terms: false
     };
+
+
 
     const [formState, setFormState] = useState(defaultState);
     const [errors, setErrors] = useState({...defaultState, terms: ""});
     const [buttonDisabled, setButtonDisabled] = useState(true);
+
+    //Question: Could you fill it in for only some of the form or would that mess things up?
+    //Also, this isn't really used anywhere in the example and that's confusing. Its defined but not used.
+    let formSchema = yup.object().shape({
+        name: yup.string().required("Please enter your name."),
+        email: yup.string().required("Please enter your email").email("This is not a valid email."),
+        password: yup.string().min(6, "Passwords must be at least 6 characters in length").required("Password is required"),
+        terms: yup
+        .boolean()
+        .oneOf([true], "You must accept Terms and Conditions")
+
+
+    });
+
+    useEffect(() => {
+       //Don't quite follow this
+        formSchema.isValid(formState).then(valid => {
+            setButtonDisabled(!valid);
+        });
+
+    }, [formState]);
+
+    //I'm unclear how this syncs up with the formSchema and things determined there, if it does at all. It seems like it's defining the problems seperately
+    const inputChange = e => {
+
+        e.persist();
+
+        yup
+        .reach(formSchema, e.target.name)
+        .validate(e.target.value)
+        .then(valid => {
+          setErrors({
+              ...errors,
+              [e.target.name]: ""
+          }); 
+        })
+        // Why err.errors [0] here?
+        .catch(err => {
+            setErrors({...errors, [e.target.name]: err.errors[0]
+            });
+        });
+
+        //Why setting it again here?
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value
+        });
+
+
+    };
+
+
+    const handleChange = e => {
+        
+
+        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        
+        setFormState({
+            ...formState, [e.target.name]: value
+        });
+       
+        
+        //check to make sure this is right b/c it might not be
+        inputChange(e);
+    };
+   
+
+    const submitData =  e => {
+        e.preventDefault();
+        console.log("Submitted");
+
+    };
+
+
 
 
   return (
@@ -29,33 +103,37 @@ function Form() {
 
 
 
-      <form onSubmit={event => onSubmit(event)}>
+      <form onSubmit={submitData}>
                 <label>
                     Name:
                     <input
                      type = "text"
                      name="name"
-                    //  value={props.values.name}
-                     onChange = {event => handleChange(event)}
+                     value={formState.name}
+                     onChange = {handleChange}
                      />
+                     {errors.name.length > 0 ? (<p className="error">{errors.name}</p>) : null}
                 </label>
+                
                 <label>
                     Email:
                     <input
                      type = "text"
                      name="email"
-                    //  value={props.values.email}
-                     onChange = {event => handleChange(event)}
+                     value={formState.email}
+                     onChange = {handleChange}
                      />
+                     {errors.email.length > 0 ? (<p className="error">{errors.email}</p>) : null}
                 </label>
                 <label>
                     Password:
                     <input
                      type = "text"
                      name="password"
-                    //  value={props.values.password}
-                     onChange = {event => handleChange(event)}
+                     value={formState.password}
+                     onChange = {handleChange}
                      />
+                     {errors.password.length > 0 ? (<p className="error">{errors.password}</p>) : null}
                 </label>
                 <label>
                     Terms and Conditions:
